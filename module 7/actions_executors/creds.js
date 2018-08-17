936400b2f3108c825502b982c86a6849b89d5fd2
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-08-17 11:38:16 +0300

    removed creds files

--- module 7/actions_executors/creds.js
+++
@@ -1,8 +0,0 @@
-const credendtials = {
-    userName: "aliaksandr_khatsko@epam.com",
-    firstName: "QAAuto_FirstName",
-    lastName: "QAAuto_SecondName",
-    email: "aliaksandr_khatsko@epam.com",
-    role: "ADMIN",
-}
-module.exports = credendtials;
