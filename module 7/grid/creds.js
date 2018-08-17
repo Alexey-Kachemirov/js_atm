a18abf3b84a80a195656f2fbbf5490fc67f345a6
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-08-17 11:16:27 +0300

    added task for module 7 with grid

---
+++ module 7/grid/creds.js
@@ -0,0 +1,8 @@
+const credendtials = {
+    userName: "aliaksandr_khatsko@epam.com",
+    firstName: "QAAuto_FirstName",
+    lastName: "QAAuto_SecondName",
+    email: "aliaksandr_khatsko@epam.com",
+    role: "ADMIN",
+}
+module.exports = credendtials;
---
+++ module 7/grid/hub_3.14.0.bat
@@ -0,0 +1,3 @@
+java -jar selenium-server-standalone-3.14.0.jar -role hub
+
+
