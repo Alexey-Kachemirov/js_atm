464637ec0fc5ff1b8bb0c67a07d89d391a979c40
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-08-24 17:48:39 +0300

    update login page steps

--- module 8/test/stepDefinitions/login.page.steps.js
+++ module 8/test/stepDefinitions/login.page.steps.js
@@ -1,25 +1,18 @@
 /* global browser,expect,assert */
 import { defineSupportCode } from 'cucumber';
 import loginPage from '../pageobjects/login.page';
-import { loginSelectors } from '../pageobjects/loginSelectors';
-import openWebsite from '../support/openWebsite';
 const creds = require('../pageobjects/creds');
 
 defineSupportCode(({ Given, Then, When }) => {
-  Given(
-    /^I open the page "([^"]*)?"$/,
-    openWebsite
-  );
-
   Given(/^I login to Portal$/, () => {
     browser.url(`http://localhost:7000/`);
-    loginSelectors.waitPageReady();
-    loginSelectors.userNameInput.setValue(creds.userName);
-    loginSelectors.firstNameInput.setValue(creds.firstName);
-    loginSelectors.lastNameInput.setValue(creds.lastName);
-    loginSelectors.emailInput.setValue(creds.email);
-    loginSelectors.roleSelect.selectByVisibleText(creds.role);
-    loginSelectors.signInButton.click();
+    loginPage.waitPageReady();
+    loginPage.userNameInput.setValue(creds.userName);
+    loginPage.firstNameInput.setValue(creds.firstName);
+    loginPage.lastNameInput.setValue(creds.lastName);
+    loginPage.emailInput.setValue(creds.email);
+    loginPage.roleSelect.selectByVisibleText(creds.role);
+    loginPage.signInButton.click();
   });
 
 });
