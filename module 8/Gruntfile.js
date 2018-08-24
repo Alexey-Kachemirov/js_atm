7a4104dbc6bf6e4f7ecbcff6fb725abaecb66510
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-08-24 10:58:16 +0300

    update config files

--- module 8/Gruntfile.js
+++ module 8/Gruntfile.js
@@ -1,19 +1,28 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        webdriver: {
-            tests: {
-                configFile: './test/config/suite.cucumber.conf.js'
-            },
-            tests-mobile: {
-                configFile: './test/config/suite.appium.conf.js'
-            },
-        },
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    webdriver: {
+      tests: {
+        configFile: './wdio.cucumber.conf'
+      },
+    },
+    clean: {
+      allureResults: ['./test/reports/allure-results/*'],
+      errorShots: ['./test/reports/errorShots/*'],
+      jsonResults: ['./test/reports/json-results/*'],
+      junitResults: ['./test/reports/junit-results/*'],
+    },
+  });
 
-    grunt.loadNpmTasks('grunt-cucumberjs');
-    grunt.loadNpmTasks('grunt-webdriver');
-    grunt.registerTask('default', ['webdriver:tests']);
-    //grunt.registerTask('default', ['webdriver:tests-mobile']);
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-cucumberjs');
+  grunt.loadNpmTasks('grunt-webdriver');
+  grunt.registerTask('default', [
+    'clean:allureResults',
+    'clean:errorShots',
+    'clean:jsonResults',
+    'clean:junitResults',
+    'webdriver:tests',
+  ]);
 };
