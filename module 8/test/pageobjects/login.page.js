2ee606bc86e6c9125947a8b58c446608707157fb
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-08-24 17:47:32 +0300

    combine 2 page objects into 1 logical page object

--- module 8/test/pageobjects/login.page.js
+++ module 8/test/pageobjects/login.page.js
@@ -2,20 +2,36 @@
 const Page = require('./page');
 
 const selectors = {
-  logoImage: '[class$="header_logo"]',
+  userNameInput: '#userName',
+  firstNameInput: '#firstName',
+  lastNameInput: '#lastName',
+  emailInput: '#email',
+  roleSelect: '#role',
+  signInButton: '.btn-primary',
 };
 
 class LoginPage extends Page {
   get selectors() { return selectors; }
-  get logoImage() { return browser.element(selectors.logoImage); }
+  get userNameInput() { return browser.element(selectors.userNameInput); }
+  get firstNameInput() { return browser.element(selectors.firstNameInput); }
+  get lastNameInput() { return browser.element(selectors.lastNameInput); }
+  get emailInput() { return browser.element(selectors.emailInput); }
+  get roleSelect() { return browser.element(selectors.roleSelect); }
+  get signInButton() { return browser.element(selectors.signInButton); }
 
   waitPageReady() {
     const checkObjects = {
-      expectedItems: [this.signInProfilesBlock, this.logoImage],
-      notExpectedItems: [super.loadingResultWithText],
+      expectedItems: [
+        this.userNameInput,
+        this.firstNameInput,
+        this.lastNameInput,
+        this.emailInput,
+        this.roleSelect,
+        this.signInButton],
     };
     super.waitPageReady(checkObjects);
     return true;
   }
+
 }
 module.exports = new LoginPage();
