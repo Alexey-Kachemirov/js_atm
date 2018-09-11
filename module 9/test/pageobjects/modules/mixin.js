ec257e6a658d0cd464a67acdcec741d71ddb728f
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-09-11 13:27:33 +0300

    Revert "add modules page objects"
    This reverts commit 98e3c8c3d06d77d88ee8dad508f0df4a1109d24a.

--- module 9/test/pageobjects/modules/mixin.js
+++
@@ -1,8 +0,0 @@
-const Mixin = Sup => class extends Sup {
-  getPageTitle() {
-    return browser.element('.//h1[contains(@class, "header_root")]').getText();
-  }
-
-}
-
-module.exports = Mixin;
--- module 9/test/stepDefinitions/home.page.steps.js
+++
@@ -1,9 +0,0 @@
-/* global browser,expect,assert */
-import { defineSupportCode } from 'cucumber';
-import homePage from '../pageobjects/home.page';
-
-defineSupportCode(({ Given, Then, When }) => {
-  Then(/^I expect to be on \[Home] page$/, () => {
-    expect(homePage.waitPageReady()).is.equal(true);
-  });
-});
