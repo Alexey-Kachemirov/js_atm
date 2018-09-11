7391bf699b67f5678c726b8ca5f852566c685111
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-09-11 13:38:52 +0300

    add step definitions files for module 9 home task

---
+++ module 9/test/stepDefinitions/controls/dropdown.steps.js
@@ -0,0 +1,17 @@
+/* global browser,expect,assert */
+import { defineSupportCode } from 'cucumber';
+import dropdown from '../../pageObjects/controls/dropdown';
+const config = require('../../../wdio.cucumber.conf').config;
+
+defineSupportCode(({ Given, Then, When }) => {
+  Then(/^I expect "(.*)" value is selected in "(.*)" dropdown$/, (expected, label) => {
+    dropdown.dropdownByLabel(label).waitForVisible();
+    browser.waitUntil(() => {
+      return dropdown.getSelectedValue(label) === expected;
+    }, config.waitforTimeout, `Exp: ${expected}\nAct:${dropdown.getSelectedValue(label)}`);
+  });
+
+  When(/^I select \"([^\"]*)\" value in "(.*)" dropdown$/, (value, label) => {
+    dropdown.selectValueFromDropdown(label, value);
+  });
+});
