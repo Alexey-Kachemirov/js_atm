3c90ce5992e9acbe466bd2a04ea675d358e97fa3
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-09-11 13:30:05 +0300

    remove all files from home task 9

--- module 9/test/support/checkStatus.js
+++
@@ -1,9 +0,0 @@
-/* global browser,expect,assert */
-import { getElementStatus } from './getElementStatus';
-import { config } from '../../wdio.cucumber.conf';
-
-export const checkStatus = (element, state, falseCase) => {
-  browser.waitUntil(() => {
-    return getElementStatus(element, state) === (!falseCase);
-  }, config.waitforTimeout, `Exp: ${state}=${!falseCase}\nAct:${getElementStatus(element, state)}`, 500);
-};
